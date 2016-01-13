ab8d1be8f14c6ba0122eb87dcc59e070decbffec
tangqilong <506620039@qq.com>
2016-01-13 20:34:47 +0800

    Initial commit jike

---
+++ six_weeks/calculator/calculator.js
@@ -0,0 +1,188 @@
+﻿var num=0;  //放置输入的值
+var result=0;   //保留输入的值
+var numshow="0";    //输出的值
+var operate=0; //判断输入状态
+var calcul=0; //判断计算状态
+var quit=0; //防止按键的重复
+
+//输入数字
+var num0 = document.getElementById("num0");
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var num3 = document.getElementById("num3");
+var num4 = document.getElementById("num4");
+var num5 = document.getElementById("num5");
+var num6 = document.getElementById("num6");
+var num7 = document.getElementById("num7");
+var num8 = document.getElementById("num8");
+var num9 = document.getElementById("num9");
+num0.onclick = function inpOutput(){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(0);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num1.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(1);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num2.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(2);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num3.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(3);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num4.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(4);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num5.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(5);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num6.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(6);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num7.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(7);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num8.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(8);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+num9.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "") : "";
+    str=str + String(9);
+    document.getElementById("text").value=str;
+    operate=0;
+    quit=0;
+}
+
+//AC键
+var ac = document.getElementById("ac");
+ac.onclick = function (){
+    num=0;
+    result=0;
+    numshow="0";
+    document.getElementById("text").value="0";
+}
+
+//小数点
+var doc = document.getElementById("doc");
+doc.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? ((operate==0) ? str : "0") : "0";
+    for(i=0; i<=str.length;i++){
+        if(str.substr(i,1)==".") return false;
+    }
+    str=str + ".";
+    document.getElementById("text").value=str;
+    operate=0;
+}
+
+//退格
+var del = document.getElementById("del");
+del.onclick = function (){
+    var str=document.getElementById("text").value+"";
+    str=(str!="0") ? str : "";
+    str=str.substr(0,str.length-1);
+    str=(str!="") ? str : "0";
+    document.getElementById("text").value=str;
+}
+
+//加法
+var plus = document.getElementById("plus");
+plus.onclick = function (){
+    calculate(); //调用计算函数
+    operate=1; //更改输入状态
+    calcul=1; //更改计算状态为加
+}
+//减法
+var minus = document.getElementById("minus");
+minus.onclick = function (){
+    calculate();
+    operate=1;
+    calcul=2;
+}
+//乘法
+var times = document.getElementById("times");
+times.onclick = function (){
+    calculate();
+    operate=1;
+    calcul=3;
+}
+//除法
+var divide = document.getElementById("divide");
+divide.onclick = function (){
+    calculate();
+    operate=1;
+    calcul=4;
+}
+//等于
+var equal = document.getElementById("equal");
+equal.onclick = function (){
+    calculate();
+    operate=1;
+    num=0;
+    result=0;
+    numshow="0";
+}
+
+function calculate(){
+    numshow=Number(document.getElementById("text").value);
+    if(num!=0 && quit!=1){ //判断前一个运算数是否为零以及防重复按键的状态
+        switch(calcul){ //判断要输入状态
+            case 1:result=num+numshow;break; //计算"+"
+            case 2:result=num-numshow;break; //计算"-"
+            case 3:result=num*numshow;break;
+            case 4:if(numshow!=0){result=num/numshow;}else{document.getElementById("note").innerHTML="被除数不能为零！"; setTimeout(clearnote,4000)} break;
+            case 5:result=num%numshow;break;
+        }
+        quit=1; //避免重复按键
+    }
+    else{
+        result=numshow;
+    }
+    numshow=String(result);
+    document.getElementById("text").value=numshow;
+    num=result; //存储当前值
+}
---
+++ six_weeks/webskin/hao123.js
@@ -0,0 +1,83 @@
+﻿//颜色按钮
+var yellow = document.getElementById("yellow"); //黄色按钮
+var red = document.getElementById("red");   //红色按钮
+var grey = document.getElementById("grey");     //灰色按钮
+var pink = document.getElementById("pink");    //粉色按钮
+var orange = document.getElementById("orange");  //橘色按钮
+//需要改变颜色的元素
+var font_color = document.getElementsByClassName("menu-color-green");   //字体颜色
+var nav_first = document.getElementsByClassName("first");   //li标签
+console.log(nav_first);
+var nav_font = document.getElementById("nav-font"); //li标签字体颜色
+var js_bg = document.getElementsByClassName("js-bg");   //导航条背景颜色
+
+yellow.onclick = function color_yellow(){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = "#a4a400";
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = "#a4a400";
+    }
+    nav_first[0].style.backgroundColor = "#a4a400";
+    nav_font.style.color = "white";
+    localStorage.setItem("color","#a4a400");
+}
+
+red.onclick = function color_red(){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = "red";
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = "red";
+    }
+    nav_first[0].style.backgroundColor = "red";
+    nav_font.style.color = "white";
+    localStorage.setItem("color","red");
+}
+
+grey.onclick = function color_grey(){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = "grey";
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = "grey";
+    }
+    nav_first[0].style.backgroundColor = "grey";
+    nav_font.style.color = "white";
+    localStorage.setItem("color","grey");
+}
+
+pink.onclick = function color_pink(){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = "#c8959d";
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = "#c8959d";
+    }
+    nav_first[0].style.backgroundColor = "#c8959d";
+    nav_font.style.color = "white";
+    localStorage.setItem("color","#c8959d");
+}
+
+orange.onclick = function color_orange(){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = "orange";
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = "orange";
+    }
+    nav_first[0].style.backgroundColor = "orange";
+    nav_font.style.color = "white";
+    localStorage.setItem("color","orange");
+}
+
+var fun = function (){
+    for(i = 0;i<font_color.length;i++){
+        font_color[i].style.color = localStorage.getItem("color");
+    }
+    for(i = 0;i<js_bg.length;i++){
+        js_bg[i].style.color = localStorage.getItem("color");
+    }
+    nav_first[0].style.backgroundColor = localStorage.getItem("color");
+    nav_font.style.color = "white";
+}();
